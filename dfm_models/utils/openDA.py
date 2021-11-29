894dbe749025c842bb0d4fed7919f50c92164046
Cody Johnson <mail@codyjohnson.xyz>
2021-11-29 16:17:44 -0600

    basic writers for openDA observations

--- dfm_models/utils/openDA.py
+++ dfm_models/utils/openDA.py
@@ -4,25 +4,51 @@ cody.l.johnson@erdc.dren.mil
 
 """
 
+from datetime import datetime as dt
 
-def writeNoosTs(data, Location, Unit, fn):
+
+def writeNoosTs(data, Location, x, y, Unit, fn):
     """write time series data in OpenDA ExchangeObject format (NOOS)
 
     data = pd.Series with datatime axis and value
 
     """
+    now = dt.now().strftime("%Y-%m-%d %H:%M")
 
     with open(fn, "w") as f:
-        f.write("#------------------------------------------------------\n")
-        f.write("#------------------------------------------------------\n")
-        f.write(f"# Location : {Location}\n")
-        f.write("# Position : (0.0,0.0)\n")
-        f.write("# Source : null\n")
-        f.write(f"# Unit : {Unit}\n")
-        f.write("# Analyse time : null\n")
-        f.write("# Timezone : null\n")
-        f.write("#------------------------------------------------------\n")
+        f.write(f"#======================================================\n")
+        f.write(f"# Generated on {now} \n")
+        f.write(f"#======================================================\n")
+        f.write(f"# Location    : {Location}\n")
+        f.write(f"# Position    : ({x:.05f},{y:.05f})\n")
+        f.write(f"# Source      : observed\n")
+        f.write(f"# Unit        : {Unit}\n")
+        f.write(f"# Analyse time: null\n")
+        f.write(f"# Timezone    : GMT\n")
+        f.write("#======================================================\n")
 
         for time, value in data.iteritems():
             strftime = time.strftime("%Y%m%d%H%M")
             f.write(f"{strftime}\t{value}\n")
+
+
+def createNoosConfigFile(stdev, stationNames, fn):
+
+    with open(fn, "w") as f:
+        f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+        f.write(
+            '<noosObserver xmlns="http://www.openda.org" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.openda.org http://schemas.openda.org/observation/noosObservations.xsd">\n'
+        )
+        f.write("\n")
+
+        for stationName in stationNames:
+            writeNoosObserver(f, stdev, stationName)
+
+        f.write("\n")
+        f.write("</noosObserver>")
+
+
+def writeNoosObserver(f, stdev, noosFn):
+    f.write(f'\t<timeSeries status="status" standardDeviation="{stdev:.02f}">\n')
+    f.write(f"\t\t{noosFn}\n")
+    f.write(f"\t</timeSeries>\n")
