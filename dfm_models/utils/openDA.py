271c44248adf473e7a5baa86fd3f2a0d453bf294
Cody Johnson <mail@codyjohnson.xyz>
2021-12-30 11:51:35 -0600

    fix

--- dfm_models/utils/openDA.py
+++ dfm_models/utils/openDA.py
@@ -7,7 +7,7 @@ cody.l.johnson@erdc.dren.mil
 from datetime import datetime as dt
 
 
-def writeNoosTs(data, Location, x, y, Unit, stdev, fn):
+def writeNoosTs(data, Location, x, y, Unit, fn):
     """write time series data in OpenDA ExchangeObject format (NOOS)
 
     data = pd.Series with datatime axis and value
@@ -19,12 +19,11 @@ def writeNoosTs(data, Location, x, y, Unit, stdev, fn):
         f.write("#======================================================\n")
         f.write(f"# Generated on {now} \n")
         f.write("#======================================================\n")
-        f.write(f"# Location    : {Location}\n")
-        f.write(f"# Position    : ({x:.05f},{y:.05f})\n")
-        f.write("# Source      : observed\n")
-        f.write(f"# Unit        : {Unit}\n")
-        f.write("# Analyse time: null\n")
-        f.write(f"standarddeviation  = {stdev:.02f}\n")
+        f.write(f"# Location         : {Location}\n")
+        f.write(f"# Position         : ({x:.05f},{y:.05f})\n")
+        f.write("# Source           : observed\n")
+        f.write(f"# Unit             : {Unit}\n")
+        f.write("# Analyse time     : null\n")
         f.write("#======================================================\n")
 
         for time, value in data.iteritems():
